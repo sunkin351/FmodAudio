d634ae685a5f749c7c8e95853f928aeab5626a12
Unknown <17joshuanewcomb@gmail.com>
2019-10-25 11:00:02 -0700

    Expose DSPMeteringInfo fully

--- FmodAudio/Dsp/DSPMeteringInfo.cs
+++ FmodAudio/Dsp/DSPMeteringInfo.cs
@@ -8,12 +8,9 @@ namespace FmodAudio.Dsp
     [StructLayout(LayoutKind.Sequential)]
     public unsafe struct DSPMeteringInfo
     {
-        public int NumSamples;
-        private fixed float peakLevel[32];
-        private fixed float rmsLevel[32];
-        public short NumChannels;
-
-        public Span<float> PeakLevel => MemoryMarshal.CreateSpan(ref peakLevel[0], 32);
-        public Span<float> RMSLevel => MemoryMarshal.CreateSpan(ref rmsLevel[0], 32);
+        public int SampleCount;
+        public fixed float peakLevel[32];
+        public fixed float rmsLevel[32];
+        public short ChannelCount;
     }
 }
