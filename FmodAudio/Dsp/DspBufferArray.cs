e1a47c4b2061aa3fbe594fc2110f249821588212
Unknown <17joshuanewcomb@gmail.com>
2019-10-25 10:08:59 -0700

    Allow Raw Pointer Access to DspBufferArray

--- FmodAudio/Dsp/DspBufferArray.cs
+++ FmodAudio/Dsp/DspBufferArray.cs
@@ -1,37 +1,17 @@
-﻿using System;
-using System.Diagnostics;
+﻿#pragma warning disable CA1815
+
+using System;
+using System.Runtime.InteropServices;
 
 namespace FmodAudio.Dsp
 {
-    public class DspBufferArray
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct DspBufferArray
     {
         public int BufferCount;
-        public int[] BufferChannelCount;
-        public ChannelMask[] BufferChannelMask;
-        public IntPtr[] Buffers;
+        public int* BufferChannelCount;
+        public ChannelMask* BufferChannelMask;
+        public IntPtr* Buffers;
         public SpeakerMode SpeakerMode;
-
-        internal unsafe DspBufferArray(ref Interop.DspBufferArrayInterop bArr)
-        {
-            int count = bArr.BufferCount;
-            BufferCount = count;
-            SpeakerMode = bArr.SpeakerMode;
-
-            if (count > 0)
-            {
-                Debug.Assert(bArr.BufferChannelCount != IntPtr.Zero && bArr.BufferChannelMask != IntPtr.Zero, "Null variable in DspBufferArrayInterop struct");
-                
-                BufferChannelCount = new int[count];
-                new Span<int>(bArr.BufferChannelCount.ToPointer(), count).CopyTo(BufferChannelCount);
-
-                BufferChannelMask = new ChannelMask[count];
-                new Span<ChannelMask>(bArr.BufferChannelMask.ToPointer(), count).CopyTo(BufferChannelMask);
-
-                Buffers = new IntPtr[count];
-                new Span<IntPtr>(bArr.Buffers.ToPointer(), count).CopyTo(Buffers);
-            }
-        }
-
-        public bool IsEmpty => BufferCount == 0;
     }
 }
