318abb7f224c97dd5b4c298d91d9d827a973ef0f
Unknown <17joshuanewcomb@gmail.com>
2019-11-07 15:43:50 -0800

    Modified AsyncReadInfo structure semantics

--- FmodAudio/AsyncReadInfo.cs
+++ FmodAudio/AsyncReadInfo.cs
@@ -8,23 +8,23 @@ namespace FmodAudio
     [StructLayout(LayoutKind.Sequential)]
     public struct AsyncReadInfo
     {
-        public IntPtr Handle;                     /* [r] The file handle that was filled out in the open callback. */
-        public uint Offset;                     /* [r] Seek position, make sure you read from this file offset. */
-        public uint SizeBytes;                  /* [r] how many bytes requested for read. */
-        public int Priority;                   /* [r] 0 = low importance.  100 = extremely important (ie 'must read now or stuttering may occur') */
+        public readonly IntPtr Handle;                     /* [r] The file handle that was filled out in the open callback. */
+        public readonly uint Offset;                     /* [r] Seek position, make sure you read from this file offset. */
+        public readonly uint SizeBytes;                  /* [r] how many bytes requested for read. */
+        public readonly int Priority;                   /* [r] 0 = low importance.  100 = extremely important (ie 'must read now or stuttering may occur') */
 
-        public IntPtr Userdata;                   /* [r] User data pointer. */
-        public IntPtr Buffer;                     /* [w] Buffer to read file data into. */
+        public readonly IntPtr Userdata;                   /* [r] User data pointer. */
+        public readonly IntPtr Buffer;                     /* [w] Buffer to read file data into. */
         public uint BytesRead;                  /* [w] Fill this in before setting result code to tell FMOD how many bytes were read. */
-        private AsyncReadInfoDoneCallback Done;  /* [r] FMOD file system wake up function.  Call this when user file read is finished.  Pass result of file read as a parameter. */
+        private readonly AsyncReadInfoDoneCallback _done;  /* [r] FMOD file system wake up function.  Call this when user file read is finished.  Pass result of file read as a parameter. */
 
-        public void InvokeDoneCallback(Result result)
+        public Result Done(Result result)
         {
-            Done.Invoke(ref this, result).CheckResult();
+            return _done.Invoke(ref this, result);
         }
 
         [StructLayout(LayoutKind.Sequential)]
-        public readonly struct AsyncReadInfoDoneCallback
+        private readonly struct AsyncReadInfoDoneCallback
         {
             private delegate Result Delegate(IntPtr funcPtr, ref AsyncReadInfo info, Result asyncResult);
             private static readonly Delegate InvokeImpl = DelegateILGeneration.GenerateCalli<Delegate>();
